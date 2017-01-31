fbad6dbb7164d86fc4c4faca737ba4e04bf87f0b
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-01-31 16:00:01 +0800

    Initial Commit

---
+++ Stand Minus WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Stand Minus WatchKit Extension
+//
+//  Created by 肇鑫 on 2017-1-31.
+//  Copyright © 2017年 ParusSoft.com. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
