8fc1879ed2d6f3988dcb83f5ba2611a03d2a95f9
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-02-01 12:53:14 +0800

    * added info.plist for HealthKit

---
+++ Stand Minus WatchKit Extension/BudgetManager.swift
@@ -0,0 +1,9 @@
+//
+//  BudgetManager.swift
+//  Stand Minus
+//
+//  Created by 肇鑫 on 2017-1-31.
+//  Copyright © 2017年 ParusSoft.com. All rights reserved.
+//
+
+import Foundation
---
+++ Stand Minus WatchKit Extension/TaskManager.swift
@@ -0,0 +1,9 @@
+//
+//  TaskManager.swift
+//  Stand Minus
+//
+//  Created by 肇鑫 on 2017-1-31.
+//  Copyright © 2017年 ParusSoft.com. All rights reserved.
+//
+
+import Foundation
