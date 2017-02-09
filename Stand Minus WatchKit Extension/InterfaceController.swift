f27e832ac643eb8eaf6338e67cff4c39731be9df
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-02-09 19:59:23 +0800

    * query run once when app first starts after rebooting.

--- Stand Minus WatchKit Extension/InterfaceController.swift
+++ Stand Minus WatchKit Extension/InterfaceController.swift
@@ -15,7 +15,7 @@ class InterfaceController: WKInterfaceController {
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        // Configure interface objects here.
+        delegate.queryStandup(at: Date(), shouldArrangeBackgroundTask: false) // run first time after reboot
     }
     
     override func willActivate() {
