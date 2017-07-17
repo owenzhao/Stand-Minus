6914238179b0f2d2401e9acee04ca06fb63e30b5
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-07-17 09:37:31 +0800

    * refactored codes

--- Stand Minus WatchKit Extension/StandHourQuery.swift
+++ Stand Minus WatchKit Extension/StandHourQuery.swift
@@ -38,111 +38,7 @@ class StandHourQuery {
     var complicationShouldReQuery = true
     private var delegate:StandHourQueryDelegate!
     
-    func start(at now:Date, hasComplication:Bool, completeHandler: @escaping () -> ()) {
-        func arrangeNextBackgroundTaskWhenDeviceIsLocked() {
-            func nextWholeHour( cps:inout DateComponents) {
-                cps.hour! += 1
-                cps.minute = 0
-            }
-
-            var cps = cal.dateComponents([.year, .month, .day, .hour, .minute], from: now)
-            
-            if hasComplication {
-                switch cps.hour! {
-                case 0..<6:
-                    nextWholeHour(cps: &cps)
-                case 6..<12:
-                    switch cps.minute! {
-                    case 0..<20:
-                        cps.minute = 20
-                    case 20..<40:
-                        cps.minute = 40
-                    default: // 40 - 60
-                        nextWholeHour(cps: &cps)
-                    }
-                default: // 12...23
-                    switch cps.minute! {
-                    case 0..<20:
-                        cps.minute = 20
-                    case 20..<40:
-                        cps.minute = 40
-                    case 40..<50:
-                        cps.minute = 50
-                    default: // 50 - 60
-                        nextWholeHour(cps: &cps)
-                    }
-                }
-            }
-            else {
-                switch cps.hour! {
-                case 0..<12:
-                    cps.hour = 12
-                    cps.minute = 50
-                case 12..<23:
-                    switch cps.minute! {
-                    case 0..<50:
-                        cps.minute = 50
-                    default: // 50 - 60
-                        nextWholeHour(cps: &cps)
-                    }
-                default: // 23
-                    switch cps.minute! {
-                    case 0..<50:
-                        cps.minute = 50
-                    default: // 50 - 60
-                        cps.day! += 1
-                        cps.hour = 12
-                        cps.minute = 50
-                    }
-                }
-            }
-            
-            let nextQueryDate = cal.date(from: cps)!
-            WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: nextQueryDate, userInfo: nil) { (error) in
-                if error == nil {
-                }
-            }
-        }
-        
-        func createPredicate() {
-            let cps = cal.dateComponents([.year, .month, .day], from: now)
-            let midnight = cal.date(from: cps)!
-            
-            predicate = HKQuery.predicateForSamples(withStart: midnight, end: midnight.addingTimeInterval(24 * 60 * 60), options: .strictStartDate)
-        }
-        
-        func creatAnchorQuery() -> HKAnchoredObjectQuery {
-            let query = HKAnchoredObjectQuery(type: sampleType, predicate: predicate, anchor: anchor, limit: HKObjectQueryNoLimit) { [unowned self] (query, samples, deletedObjects, nextAnchor, error) -> Void in
-                if error == nil {
-                    defer {
-                        self.anchor = nextAnchor
-                        completeHandler()
-                    }
-                    
-                    if self.isFirstQuery {
-                        defer { self.isFirstQuery = false }
-                        self.data.assign(samples as! [HKCategorySample])
-                    }
-                    else {
-                        if let deletedObjects = deletedObjects {
-                            self.data.delete(deletedObjects)
-                        }
-                        if let samples = samples as? [HKCategorySample] {
-                            self.data.append(samples)
-                        }
-                    }
-                }
-                else { // device is locked. **query failed, reason: Protected health data is inaccessible**
-                    self.complicationShouldReQuery = true
-                    let defaults = UserDefaults.standard
-                    defaults.removeObject(forKey: DefaultsKey.hasStoodKey)
-                    arrangeNextBackgroundTaskWhenDeviceIsLocked()
-                }
-            }
-            
-            return query
-        }
-        
+    func start(at now:Date, hasComplication:Bool, completionHandler: @escaping () -> ()) {
         defer { delegate.lastQueryDate = now }
         
         if delegate == nil {
@@ -150,7 +46,7 @@ class StandHourQuery {
         }
         else {
             guard delegate.shouldQuery(at: now) else {
-                completeHandler()
+                completionHandler()
                 return
             }
         }
@@ -161,16 +57,121 @@ class StandHourQuery {
             
             anchor = nil
             
-            createPredicate()
+            createPredicate(at: now)
         }
         
-        let anchorQuery = creatAnchorQuery()
+        let anchorQuery = creatAnchorQuery(at: now, hasComplication: hasComplication, completionHandler: completionHandler)
         
         store.requestAuthorization(toShare: nil, read: [sampleType]) { [unowned self] (success, error) in
             if error == nil && success {
                 self.store.execute(anchorQuery)
             }
         }
+    }
+    
+    
+    private func arrangeNextBackgroundTaskWhenDeviceIsLocked(at now:Date, hasComplication:Bool) {
+        func nextWholeHour( cps:inout DateComponents) {
+            cps.hour! += 1
+            cps.minute = 0
+        }
+        
+        var cps = cal.dateComponents([.year, .month, .day, .hour, .minute], from: now)
+        
+        if hasComplication {
+            switch cps.hour! {
+            case 0..<6:
+                nextWholeHour(cps: &cps)
+            case 6..<12:
+                switch cps.minute! {
+                case 0..<20:
+                    cps.minute = 20
+                case 20..<40:
+                    cps.minute = 40
+                default: // 40 - 60
+                    nextWholeHour(cps: &cps)
+                }
+            default: // 12...23
+                switch cps.minute! {
+                case 0..<20:
+                    cps.minute = 20
+                case 20..<40:
+                    cps.minute = 40
+                case 40..<50:
+                    cps.minute = 50
+                default: // 50 - 60
+                    nextWholeHour(cps: &cps)
+                }
+            }
+        }
+        else {
+            switch cps.hour! {
+            case 0..<12:
+                cps.hour = 12
+                cps.minute = 50
+            case 12..<23:
+                switch cps.minute! {
+                case 0..<50:
+                    cps.minute = 50
+                default: // 50 - 60
+                    nextWholeHour(cps: &cps)
+                }
+            default: // 23
+                switch cps.minute! {
+                case 0..<50:
+                    cps.minute = 50
+                default: // 50 - 60
+                    cps.day! += 1
+                    cps.hour = 12
+                    cps.minute = 50
+                }
+            }
+        }
+        
+        let nextQueryDate = cal.date(from: cps)!
+        WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: nextQueryDate, userInfo: nil) { (error) in
+            if error == nil {
+            }
+        }
+    }
+    
+    private func createPredicate(at now:Date) {
+        let cps = cal.dateComponents([.year, .month, .day], from: now)
+        let midnight = cal.date(from: cps)!
+        
+        predicate = HKQuery.predicateForSamples(withStart: midnight, end: midnight.addingTimeInterval(24 * 60 * 60), options: .strictStartDate)
+    }
+    
+    private func creatAnchorQuery(at now:Date, hasComplication:Bool, completionHandler:@escaping () -> ()) -> HKAnchoredObjectQuery {
+        let query = HKAnchoredObjectQuery(type: sampleType, predicate: predicate, anchor: anchor, limit: HKObjectQueryNoLimit) { [unowned self] (query, samples, deletedObjects, nextAnchor, error) -> Void in
+            if error == nil {
+                defer {
+                    self.anchor = nextAnchor
+                    completionHandler()
+                }
+                
+                if self.isFirstQuery {
+                    defer { self.isFirstQuery = false }
+                    self.data.assign(samples as! [HKCategorySample])
+                }
+                else {
+                    if let deletedObjects = deletedObjects {
+                        self.data.delete(deletedObjects)
+                    }
+                    if let samples = samples as? [HKCategorySample] {
+                        self.data.append(samples)
+                    }
+                }
+            }
+            else { // device is locked. **query failed, reason: Protected health data is inaccessible**
+                self.complicationShouldReQuery = true
+                let defaults = UserDefaults.standard
+                defaults.removeObject(forKey: DefaultsKey.hasStoodKey)
+                self.arrangeNextBackgroundTaskWhenDeviceIsLocked(at: now, hasComplication: hasComplication)
+            }
+        }
+        
+        return query
     }
 }
 
