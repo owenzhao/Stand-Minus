3b260731c87b4961d279e0bf5b9338e5deabd990
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-08-06 17:39:40 +0800

    * added MessageType.swift

---
+++ Stand Minus/MessageType.swift
@@ -0,0 +1,16 @@
+//
+//  MessageType.swift
+//  Stand Minus
+//
+//  Created by 肇鑫 on 2017-8-6.
+//  Copyright © 2017年 ParusSoft.com. All rights reserved.
+//
+
+import Foundation
+
+enum MessageType:String {
+    case newHour = "new hour"
+    case fiftyMinutes = "50 minutes"
+    case rightNow = "right now"
+}
+
