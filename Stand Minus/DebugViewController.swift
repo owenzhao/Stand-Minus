c1beec0e4d46185ab4a8595660ee32ef8a1095ca
肇鑫 Owen Zhao <owenzx@gmail.com>
2017-08-09 11:08:43 +0800

    * since watchOS side is no need to send info to iOS side, there is no need to show the status of watchOS side on iOS side as well.

--- Stand Minus/DebugViewController.swift
+++ Stand Minus/DebugViewController.swift
@@ -36,20 +36,21 @@ class DebugViewController: UIViewController {
             hasNotifiedWatchSideLabel.text = "状态未知"
         }
         
-        if  let session = (UIApplication.shared.delegate as? AppDelegate)?.session,
-            let total = session.receivedApplicationContext["total"] as? Int,
-            let hasStoodInCurrentHour = session.receivedApplicationContext["hasStoodInCurrentHour"] as? Bool,
-            let date = session.receivedApplicationContext["date"] as? Date {
+        if  let session = (UIApplication.shared.delegate as? AppDelegate)?.session {    
+//        },
+//            let total = session.receivedApplicationContext["total"] as? Int,
+//            let hasStoodInCurrentHour = session.receivedApplicationContext["hasStoodInCurrentHour"] as? Bool,
+//            let date = session.receivedApplicationContext["date"] as? Date {
             
-            totalLabel.text = String(total)
-            hasStoodInCurrentHourLabel.text = hasStoodInCurrentHour ? "已站立" : "未站立"
-            queryDateLabel.text = DateFormatter.localizedString(from: date, dateStyle: .none, timeStyle: .short)
+//            totalLabel.text = String(total)
+//            hasStoodInCurrentHourLabel.text = hasStoodInCurrentHour ? "已站立" : "未站立"
+//            queryDateLabel.text = DateFormatter.localizedString(from: date, dateStyle: .none, timeStyle: .short)
             remainCountsLabel.text = String(session.remainingComplicationUserInfoTransfers)
         }
         else {
-            totalLabel.text = "未知"
-            hasStoodInCurrentHourLabel.text = "未知"
-            queryDateLabel.text = "未知"
+//            totalLabel.text = "未知"
+//            hasStoodInCurrentHourLabel.text = "未知"
+//            queryDateLabel.text = "未知"
             remainCountsLabel.text = "未知"
         }
         
@@ -66,9 +67,9 @@ class DebugViewController: UIViewController {
     
     @IBOutlet weak var remoteNotificationArrivingDateLabel: UILabel!
     @IBOutlet weak var hasNotifiedWatchSideLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var hasStoodInCurrentHourLabel: UILabel!
-    @IBOutlet weak var queryDateLabel: UILabel!
+//    @IBOutlet weak var totalLabel: UILabel!
+//    @IBOutlet weak var hasStoodInCurrentHourLabel: UILabel!
+//    @IBOutlet weak var queryDateLabel: UILabel!
     @IBOutlet weak var remainCountsLabel: UILabel!
     /*
     // MARK: - Navigation
