25716618ad4e81c31b5ce32271de9888d081552b
肇鑫（Zhao Xin） <owenzx@gmail.com>
2019-10-11 15:00:17 +0800

    * added new file ComplicationModel.swift.

---
+++ Stand Minus WatchKit Extension/ComplicationModel.swift
@@ -0,0 +1,9 @@
+//
+//  ComplicationModel.swift
+//  Stand Minus WatchKit Extension
+//
+//  Created by zhaoxin on 2019/10/11.
+//  Copyright © 2019 ParusSoft.com. All rights reserved.
+//
+
+import Foundation
