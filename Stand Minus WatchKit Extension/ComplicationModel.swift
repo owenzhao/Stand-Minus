1967ab6042440759be61c0a8a597108ec55674a5
肇鑫（Zhao Xin） <owenzx@gmail.com>
2019-10-12 19:19:20 +0800

    * implemented ComplicationModel.swift.

--- Stand Minus WatchKit Extension/ComplicationModel.swift
+++ Stand Minus WatchKit Extension/ComplicationModel.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import HealthKit
+
+struct ComplicationModel {
+    static var today:Date? = nil
+    
+    let queryDate:Date
+    let total:Int?
+    let hasStoodInCurrentHour:Bool?
+    
+    var minute:Int {
+        return StandHourQuery.calendar.component(.minute, from: queryDate)
+    }
+    
+    init(queryDate:Date, samples:[HKCategorySample]) {
+        self.queryDate = queryDate
+        
+        if let lastSample = samples.last {
+            let total = samples.reduce(0) { (result, sample) -> Int in
+                result + (1 - sample.value)
+            }
+            
+            var hasStoodInCurrentHour = false
+            
+            if lastSample.value == HKCategoryValueAppleStandHour.stood.rawValue {
+                let currentHour = StandHourQuery.calendar.component(.hour, from: queryDate)
+                let lastSampleHour = StandHourQuery.calendar.component(.hour, from: lastSample.startDate)
+                hasStoodInCurrentHour = (currentHour == lastSampleHour)
+            }
+            
+            self.total = total
+            self.hasStoodInCurrentHour = hasStoodInCurrentHour
+        }
+        else {
+            self.total = 0
+            self.hasStoodInCurrentHour = false
+        }
+    }
+    
+    init(queryDate:Date) {
+        self.queryDate = queryDate
+        self.total = nil
+        self.hasStoodInCurrentHour = nil
+    }
+}
