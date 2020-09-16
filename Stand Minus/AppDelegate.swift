d0806bb7684276e442049c0ff6d4e672b342fa89
肇鑫（Zhao Xin） <owenzx@gmail.com>
2020-09-16 11:02:06 +0800

    * target version to iOS 14. * update pods.

--- Stand Minus/AppDelegate.swift
+++ Stand Minus/AppDelegate.swift
@@ -228,6 +228,6 @@ extension AppDelegate:WCSessionDelegate {
 extension AppDelegate:UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
-        completionHandler([.alert, .sound])
+        completionHandler([.sound])
     }
 }
